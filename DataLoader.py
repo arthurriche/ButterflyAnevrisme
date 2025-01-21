39011eb4fd05263f4419ce3fa249876dc6f7c036
raphzagh <90769794+raphzagh@users.noreply.github.com>
2025-01-21 08:57:17 +0100

    Update DataLoader.py - Add y: speed or delta speed at t+1

--- DataLoader.py
+++ DataLoader.py
@@ -21,10 +21,10 @@ class Dataset(BaseDataset):
         self.files = os.listdir(folder_path)
         self.files = [file for file in self.files if file.endswith(".xdmf")]
         self.files.sort()
-        self.len_time = 80
+        self.len_time = 79
         self.number_files = len(self.files) * self.len_time
         self.encode_id = {i+t:(i,t) for t,i in product(range(self.len_time),range(len(self.files)))}
-        
+        self.delta_speed = False
 
 
     def __len__(self):
@@ -35,8 +35,38 @@ class Dataset(BaseDataset):
         meshes = self.xdmf_to_meshes(self.folder_path+self.files[i])
         
         mesh = meshes[t]
-            
-        #Add constant graph structure
+        
+        #Get data from mesh
+        data, pos, edges = self.mesh_to_graph_data(mesh)
+        
+        #Get speed for t+1 mesh
+        next_t_mesh = meshes[t+1]
+        next_data = self.get_speed_data(next_t_mesh)
+
+
+        if self.delta_speed:
+          next_data = next_data - data
+        #Structure the information
+        current_graph_data = {
+                              "x":data,
+                              "pos":pos,
+                              "edge_index":edges,
+                              "y":next_data,
+                              }
+        
+        graph_data = Data(x=current_graph_data['x'],
+                                pos=current_graph_data['pos'],
+                                edge_index=current_graph_data['edge_index'],
+                                y=current_graph_data['y'])
+
+        return graph_data
+    
+    def get_speed_data(self,mesh):
+        data = torch.from_numpy(np.concatenate([mesh.point_data['Vitesse'],
+                                              mesh.point_data['Pression'][:,None]],axis=1)).to(self.device)
+        return data
+    
+    def mesh_to_graph_data(self,mesh):
         node_edges = []
         for tetra in mesh.cells_dict['tetra']:
           for node,neighbor in product(tetra,tetra):
@@ -48,65 +78,54 @@ class Dataset(BaseDataset):
         wall_labels_tensor = torch.tensor(wall_labels, device=self.device).unsqueeze(1)  # Convert to tensor and add dimension
         pos = torch.cat([pos, wall_labels_tensor], dim=1)  # Concatenate with pos
         
-        data = torch.from_numpy(np.concatenate([mesh.point_data['Vitesse'],
-                                                mesh.point_data['Pression'][:,None]],axis=1)).to(self.device)
-        current_graph_data = {
-                              "x":data,
-                              "pos":pos,
-                              "edge_index":edges,
-                              }
-        
-        graph_data = Data(x=current_graph_data['x'],
-                                pos=current_graph_data['pos'],
-                                edge_index=current_graph_data['edge_index'],
-                                y=torch.tensor([i,t]))
+        data = self.get_speed_data(mesh)
 
-        return graph_data
-      
+        return data, pos, edges
+
     @staticmethod
     def classify_vertices(mesh: meshio.Mesh, velocity_key: str = "Vitesse") -> np.ndarray:
-      """
-      Classify each vertex of the mesh as being on the wall or in the flow.
+        """
+        Classify each vertex of the mesh as being on the wall or in the flow.
 
-      Parameters:
-          mesh: The mesh object containing point data.
-          velocity_key: The key for the velocity data in the mesh point_data.
+        Parameters:
+            mesh: The mesh object containing point data.
+            velocity_key: The key for the velocity data in the mesh point_data.
 
-      Returns:
-          A numpy array of labels (0 for wall, 1 for flow).
-      """
-      if velocity_key not in mesh.point_data:
-          raise ValueError(f"Velocity data key '{velocity_key}' not found in mesh point_data.")
+        Returns:
+            A numpy array of labels (0 for wall, 1 for flow).
+        """
+        if velocity_key not in mesh.point_data:
+            raise ValueError(f"Velocity data key '{velocity_key}' not found in mesh point_data.")
 
-      velocities = np.array(mesh.point_data[velocity_key])  # Shape: (num_points, 3)
-      speed_norm = np.linalg.norm(velocities, axis=1)  # Compute the norm of velocity for each vertex
-      labels = np.where(speed_norm == 0, 0, 1)  # 0 for wall, 1 for flow
-      return labels
+        velocities = np.array(mesh.point_data[velocity_key])  # Shape: (num_points, 3)
+        speed_norm = np.linalg.norm(velocities, axis=1)  # Compute the norm of velocity for each vertex
+        labels = np.where(speed_norm == 0, 0, 1)  # 0 for wall, 1 for flow
+        return labels
         
     @staticmethod
     def xdmf_to_meshes(xdmf_file_path: str) -> List[meshio.Mesh]:
-      """
-      Opens an XDMF archive file, and extract a data mesh object for every timestep.
+        """
+        Opens an XDMF archive file, and extract a data mesh object for every timestep.
 
-      xdmf_file_path: path to the .xdmf file.
-      Returns: list of data mesh objects.
-      """
+        xdmf_file_path: path to the .xdmf file.
+        Returns: list of data mesh objects.
+        """
 
-      reader = meshio.xdmf.TimeSeriesReader(xdmf_file_path)
-      points, cells = reader.read_points_cells()
-      meshes = []
+        reader = meshio.xdmf.TimeSeriesReader(xdmf_file_path)
+        points, cells = reader.read_points_cells()
+        meshes = []
 
-      # Extracting the meshes from the archive
-      for i in range(reader.num_steps):
-          # Depending on meshio version, the function read_data may return 3 or 4 values.
-          try:
-              time, point_data, cell_data, _ = reader.read_data(i)
-          except ValueError:
-              time, point_data, cell_data = reader.read_data(i)
-          mesh = meshio.Mesh(points, cells, point_data=point_data, cell_data=cell_data)
-          meshes.append(mesh)
-      print(f"Loaded {len(meshes)} timesteps from {xdmf_file_path.split('/')[-1]}\n")
-      return meshes
+        # Extracting the meshes from the archive
+        for i in range(reader.num_steps):
+            # Depending on meshio version, the function read_data may return 3 or 4 values.
+            try:
+                time, point_data, cell_data, _ = reader.read_data(i)
+            except ValueError:
+                time, point_data, cell_data = reader.read_data(i)
+            mesh = meshio.Mesh(points, cells, point_data=point_data, cell_data=cell_data)
+            meshes.append(mesh)
+        print(f"Loaded {len(meshes)} timesteps from {xdmf_file_path.split('/')[-1]}\n")
+        return meshes
 
 
 folder_path = '/content/drive/MyDrive/IDSC/4Students_AnXplore03/'
