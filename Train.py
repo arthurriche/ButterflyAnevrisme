1432f43a1f6ea9234b56e77a1d7dcad5bd09c7e7
raphzagh <90769794+raphzagh@users.noreply.github.com>
2025-01-21 09:43:39 +0100

    Update Train.py - first changes

--- Train.py
+++ Train.py
@@ -2,33 +2,33 @@ from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter("tensorboard")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-dataset = Dataset()
+folder_path = '/content/drive/MyDrive/IDSC/4Students_AnXplore03/'
+dataset = Dataset(folder_path)
 
 train_loader = DataLoader(
     dataset=dataset,
     batch_size=1,
     shuffle=True,
-    num_workers=0,
+    num_workers=2,
 )
 
 model = EncodeProcessDecode(
-    node_input_size=12,
-    edge_input_size=3,
+    node_input_size=5,
+    edge_input_size=0,
     message_passing_num=15,
     hidden_size=128,
-    output_size=2,
+    output_size=3,
 ) #
 loss = L2Loss() #
 simulator = Simulator(
-    node_input_size=12,
-    edge_input_size=3,
-    output_size=2,
+    node_input_size=5,
+    edge_input_size=0,
+    output_size=3,
     feature_index_start=0,
-    feature_index_end=2,
+    feature_index_end=3,
     output_index_start=0,
-    output_index_end=2,
-    node_type_index=2,
+    output_index_end=3,
+    node_type_index=3,
     batch_size=1,
     model=model,
     device=device,
